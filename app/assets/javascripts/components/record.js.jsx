312a060b489a1e6328b0e062fc47f4b69818dc51
Xiaofan Wu <qvanwu@gmail.com>
2015-08-15 14:24:59 +0200

    clean code for Ajax

--- app/assets/javascripts/components/record.js.jsx
+++ app/assets/javascripts/components/record.js.jsx
@@ -15,64 +15,56 @@ var Record = React.createClass({
       date: React.findDOMNode(this.refs.date).value,
       amount: React.findDOMNode(this.refs.amount).value
     };
-    return $.ajax({
+    $.ajax({
       method: 'PUT',
       url: "/records/" + this.props.record.id,
       dataType: 'JSON',
-      data: {
-        record: data
-      },
-      success: (function(_this) {
-        return function(data) {
-          _this.setState({
-            edit: false
-          });
-          return _this.props.handleEditRecord(_this.props.record, data);
-        };
-      })(this)
+      data: { record: data },
+      success: function(data) {
+        this.setState({ edit: false });
+        this.props.handleEditRecord(this.props.record, data);
+      }.bind(this)
+    });
+  },
+
+  handleDelete: function (e) {
+    e.preventDefault();
+    $.ajax({
+      method: 'DELETE',
+      url: "/records/" + this.props.record.id,
+      dataType: 'JSON',
+      success: function () {
+        this.props.handleDeleteRecord(this.props.record);
+      }.bind(this)
     });
   },
 
   recordForm: function () {
     return (
-      <tr>
+        <tr>
         <td><input type="date" className="form-control" defaultValue={this.props.record.date} ref="date"/></td>
         <td><input type="text" className="form-control" defaultValue={this.props.record.title} ref="title"/></td>
         <td><input type="number" className="form-control" defaultValue={this.props.record.amount} ref="amount"/></td>
         <td>
-          <a className="btn btn-default" onClick={this.handleEdit}>Update</a>
-          <a className="btn btn-danger" onClick={this.handleToggle}>Cancel</a>
+        <a className="btn btn-default" onClick={this.handleEdit}>Update</a>
+        <a className="btn btn-danger" onClick={this.handleToggle}>Cancel</a>
         </td>
-      </tr>
-    );
+        </tr>
+        );
   },
 
   recordRow: function () {
     return (
-      <tr>
+        <tr>
         <td>{this.props.record.date}</td>
         <td>{this.props.record.title}</td>
         <td>{amountFormat(this.props.record.amount)}</td>
         <td>
-          <a className="btn btn-default" onClick={this.handleToggle}>Edit</a>
-          <a className="btn btn-danger" onClick={this.handleDelete}>Delete</a>
+        <a className="btn btn-default" onClick={this.handleToggle}>Edit</a>
+        <a className="btn btn-danger" onClick={this.handleDelete}>Delete</a>
         </td>
-      </tr>
-    );
-  },
-
-  handleDelete: function (e) {
-    e.preventDefault();
-    return $.ajax({
-      method: 'DELETE',
-      url: "/records/" + this.props.record.id,
-      dataType: 'JSON',
-      success: (function(_this) {
-        return function() {
-          return _this.props.handleDeleteRecord(_this.props.record);
-        };
-      })(this)
-    });
+        </tr>
+        );
   },
 
   render: function () {
